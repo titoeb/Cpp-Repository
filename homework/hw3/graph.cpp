e35224fd2c7ca4ff9538f1437b3d0a28dc9a7ef2
Tim Toebrock <tim.toebrock@rewe-digital.com>
2020-01-28 17:55:37 +0100

    Working on homework.

--- homework/hw3/graph.cpp
+++ homework/hw3/graph.cpp
@@ -6,6 +6,8 @@
 #include <iterator>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -13,7 +15,7 @@ float give_min(const float* min_array, const float* exclude_array, int n_nodes){
     float min = numeric_limits<float>::max();
     int min_pos = -1;
     for(int i = 0; i < n_nodes; ++i){
-        if((min_array[i] >= 0.0) && (exclude_array[i] == -1.0) && (min_array[i] < min)){
+        if((min_array[i] < numeric_limits<float>::max()) && (exclude_array[i] == numeric_limits<float>::max()) && (min_array[i] < min)){
             min = min_array[i];
             min_pos = i;
         }
@@ -21,38 +23,66 @@ float give_min(const float* min_array, const float* exclude_array, int n_nodes){
     return min_pos;
 }
 
-int argmin(float* array, int len){
+int argmin(vector<float> input){
     int min_node = 0;
-    for(int pos = 1; pos < len; ++pos){
-        if (array[pos] < array[min_node]){
-            min_node = pos;
+    float current_min = numeric_limits<float>::max();
+    for(int i = 0; i<input.size(); ++i){
+        if ((input[i] < current_min)){
+            min_node = i;
+            current_min = input[i];
         }
     }
     return min_node;
 }
 
+void process_line(int& node_a, int& node_b, float& cost, const string& line){
+    istringstream input(line);
+    string word;
+    getline(input, word, ' ');
+    node_a = stoi(word);
+
+    getline(input, word, ' ');
+    node_b = stoi(word);
+
+    getline(input, word, ' ');
+    cost = static_cast<float> (stoi(word));
+}
+
 class Graph{
     public:
-        // Constructor
-
         // Empty Constructor
         Graph(int n_nodes):n_nodes(n_nodes), verbose(0){
             // Allocate empty adjecency matrix.
-            this->adjecency_matrix = new float*[this->n_nodes];
-            for (int node = 0; node < this->n_nodes; ++node){
-                this->adjecency_matrix[node] = new float[this->n_nodes];
-                for(int element = 0; element < this->n_nodes; ++element) this->adjecency_matrix[node][element] = -1.0;
-            }
+            vector< vector<float> > a(n_nodes, vector<float>(n_nodes, numeric_limits<float>::max()));
+            this->adjecency_matrix = a;
         }
 
         // Random Constructor
+        Graph(ifstream& graph_file, int verbosity=0):verbose(verbosity){
+            string line;
+            int node_a, node_b;
+            float cost;
+            // Read n_nodes from file
+            getline(graph_file, line);
+            this->n_nodes = stoi(line);
+            
+            // Initiallize the adjecency matrix.
+            vector< vector<float> > a(n_nodes, vector<float>(n_nodes, numeric_limits<float>::max()));
+            this->adjecency_matrix = a;
+
+            // Iterator through file and read in vertices with their cost.
+            while(getline(graph_file, line)){
+                // process line
+                process_line(node_a, node_b, cost, line);
+                // enter results into adjecency matrix.
+                this->adjecency_matrix[node_a][node_b] = this->adjecency_matrix[node_b][node_a] = cost;
+            }
+        }
+
         Graph(int n_nodes, float density, float distance_range):n_nodes(n_nodes), verbose(0){
             // Initiallize the adjecency matrix.
-            this->adjecency_matrix = new float*[this->n_nodes];
-            for (int node = 0; node < this->n_nodes; ++node){
-                this->adjecency_matrix[node] = new float[this->n_nodes];
-            }
-
+            vector< vector<float> > a(n_nodes, vector<float>(n_nodes, numeric_limits<float>::max()));
+            this->adjecency_matrix = a;
             // Initiallize the random sampler to get different results each time.
             mt19937 generator (time(0));
 
@@ -70,24 +100,12 @@ class Graph{
                         this->adjecency_matrix[node_1][node_2] =  this->adjecency_matrix[node_2][node_1] = distances(generator);
                     } else {
                         // A distance of -1.0 does mean that there is no edge between two nodes.
-                        this->adjecency_matrix[node_1][node_2] =  this->adjecency_matrix[node_2][node_1] = -1.0;
+                        this->adjecency_matrix[node_1][node_2] =  this->adjecency_matrix[node_2][node_1] = numeric_limits<float>::max();
                     }
                 }
             }
         }
 
-        // Destructor
-        ~Graph(){
-            // Dealloate the dynamic array adjecency matrix.
-
-            // Deallocate all rows from adjecency matrix.
-            for (int node = 0; node < this->n_nodes; ++node){
-                delete this->adjecency_matrix[node];
-            }
-
-            // Finally release the adjecency matrix itself.
-            delete this->adjecency_matrix;
-        }
         // Set and get functions
         // Set Edge
         void set_edge(int node_1, int node_2, float distance){
@@ -103,7 +121,7 @@ class Graph{
             // go through all possible connections and print out the ones that exist.
             for(int node_1 = 0; node_1 < this->n_nodes; ++node_1){
                 for(int node_2 = node_1 + 1; node_2 < this->n_nodes; ++node_2){
-                    if (this->adjecency_matrix[node_1][node_2] >= 0.0){
+                    if (this->adjecency_matrix[node_1][node_2] != numeric_limits<float>::max()){
                         // There is an edge between node1, node2. Print it!
                         cout << node_1 << " <--" << this->adjecency_matrix[node_1][node_2] << "--> " << node_2 << endl;
                     }
@@ -121,58 +139,54 @@ class Graph{
             int min_node;
 
             // Buffer to save nodes that are already in the spanning tree or not.
-            float open_set[this->n_nodes];
-            int closed_set[this->n_nodes];
-            // Allocate initially -1
-            for(int i = 0; i < this->n_nodes; ++i){
-                open_set[i] = -1.0;
-                closed_set[i] = -1;
-            }
+            vector<float> open_set(this->n_nodes,  numeric_limits<float>::max());
+            vector<bool> closed_set(this->n_nodes, false);
+
             // Put first node in the set.
-            closed_set[0] = node_entered;
+            closed_set[0] = true;
             node_entered++;
 
             // Put all nodes reachable in the open set.
             for(int i = 0; i < this->n_nodes; i++){
-                if((this->adjecency_matrix[0][i] >= 0) && (closed_set[i] >= 0)){
+                if((this->adjecency_matrix[0][i] >= 0) && (closed_set[i] == false)){
                     // node i is reachable and not yet in the minimum spanning tree
                     dist_tmp = this->adjecency_matrix[0][i];
-                    if((dist_tmp < open_set[i]) || (open_set[i] == -1.0)){
+                    if((dist_tmp < open_set[i]) || (open_set[i] ==  numeric_limits<float>::max())){
                         open_set[i] = dist_tmp;
                         nodes_in_open++;
                     }
                 }
             }
 
-            cout << "nodes entered: " << node_entered << "nodes_in_open: " << nodes_in_open << endl;
+            cout << "nodes entered: " << node_entered << ", nodes_in_open: " << nodes_in_open << endl;
             // Iterate either until all nodes are in the closed set
             // or some are not but their are no nodes reachable in the open set.
-            while((node_entered < (this->n_nodes - 1)) && (nodes_in_open > 0)){
+            while((node_entered < this->n_nodes) && (nodes_in_open > 0)){
                 // Search for node with minimal distance in the open set.
-                min_node = argmin(open_set, this->n_nodes);
+                min_node = argmin(open_set);
 
                 // Add it to the closed set.
-                closed_set[min_node] = node_entered;
+                closed_set[min_node] = true;
                 node_entered++;
                 distance += open_set[min_node];
                 cout << "Node " << min_node << " entered the closed set with distance " << open_set[min_node] << endl;
-                open_set[min_node] = -1.0;
+                open_set[min_node] =  numeric_limits<float>::max();
                 nodes_in_open--;
 
                 // Add new available node to open set.
                 for(int i = 0; i < this->n_nodes; i++){
-                    if((this->adjecency_matrix[min_node][i] >= 0) && (closed_set[i] >= 0)){
+                    if((this->adjecency_matrix[min_node][i] >= 0) && (closed_set[i] == false)){
                         // node i is reachable and not yet in the minimum spanning tree
                         dist_tmp = this->adjecency_matrix[min_node][i];
-                        if((dist_tmp < open_set[i]) || (open_set[i] == -1.0)){
+                        if((dist_tmp < open_set[i]) || (open_set[i] == numeric_limits<float>::max())){
+                            if (open_set[i] == numeric_limits<float>::max()) nodes_in_open++;
                             open_set[i] = dist_tmp;
-                            nodes_in_open++;
                         }
                     }
                 }        
             }
             // Finally test whether the algorithm was succesful
-            if (node_entered == this->n_nodes-1){
+            if (node_entered == this->n_nodes){
                 return distance;
             } else {
                 return -1.0;
@@ -191,8 +205,8 @@ class Graph{
 
             // Make all entries to zero
             for(int i = 0; i< this->n_nodes; ++i){
-                open_set[i] = -1.0;
-                closed_set[i] = -1.0;
+                open_set[i] =  numeric_limits<float>::max();
+                closed_set[i] =  numeric_limits<float>::max();
             }
 
             // Begin with algorithm.
@@ -216,7 +230,7 @@ class Graph{
                         // 1) The reachable node is reachable from the minimal node we are looking at.
                         // 2) The reachable node is not in the closed set, then it would be irrelevant
                         // 3) Either the reachable node was not set in the open set or the path via the minimal node is smaller than the path to the reachable node that is currently in the open set.
-                        if((this->adjecency_matrix[minimal_node][reachable_node] >= 0.0) && (closed_set[reachable_node] == -1.0) && ((distance_to < open_set[reachable_node]) || open_set[reachable_node] == -1.0)){
+                        if((this->adjecency_matrix[minimal_node][reachable_node] >= 0.0) && (closed_set[reachable_node] ==  numeric_limits<float>::max()) && ((distance_to < open_set[reachable_node]) || open_set[reachable_node] == numeric_limits<float>::max())){
                             open_set[reachable_node] = distance_to;
                              // Some print out for debug
                             if (this->verbose > 0){
@@ -233,7 +247,7 @@ class Graph{
                         cout << endl;
                     }
                     
-                    open_set[minimal_node] = -1.0;
+                    open_set[minimal_node] = numeric_limits<float>::max();
                     n_open = n_open - 1;
                 }
             }
@@ -252,7 +266,7 @@ class Graph{
             for(int node = 1; node < this->n_nodes; ++node){
                 min_dist = min_distance(0, node);
                 if (min_dist != -1.0){
-                    sum_length = sum_length + min_
+                    sum_length = sum_length + min_dist;
         // Used for debug printouts. ist;
                 } else {
                     cout << "ERROR! Did not find a way from node 1 to node " << node << "." << endl;
@@ -264,12 +278,12 @@ class Graph{
     private:
         // How many nodes are in the network
         int n_nodes;
- Additional message printed if verbose > 0.
+        //Additional message printed if verbose > 0.
         int verbose;
 
         // The weights of the matrix, as the graph is undirected, the array will be symmetric.
         // Therefore, one could also implement a triangular matrix instead.
-        float** adjecency_matrix;
+        vector<vector<float>> adjecency_matrix;
 };
 
 
@@ -277,18 +291,6 @@ int main(){
     // Testing the implementaion of graph and average shortest path.
     /* 
     Graph test_graph = Graph(1000, 0.4, 100.0);
-
-    // Create a random graph and print it.
-    Graph test_graph = Graph(6);
-    test_graph.set_edge(0, 1, 7.0);
-    test_graph.set_edge(0, 2, 9.0);
-    test_graph.set_edge(0, 5, 14.0);
-    test_graph.set_edge(1, 2, 10.0);
-    test_graph.set_edge(1, 3, 15.0);
-    test_graph.set_edge(2, 3, 11.0);
-    test_graph.set_edge(2, 5, 2.0);
-    test_graph.set_edge(3, 4, 6.0);
-    test_graph.set_edge(4, 5, 9.0);
 
     //test_graph.set_verbosity(1);
     float min_dist = test_graph.average_path_length();
@@ -304,8 +306,14 @@ int main(){
     cout << test_graph.minimum_spanning_tree() << endl; */
 
     // String that will contain file_name to load graph from.
-    string file_name;
-    cout << "Please give the file from which the graph should be created!";
-    cin >> file_name;
-    cout << file_name << endl;
+    string file_name("test.txt");
+    //cout << "Please give the file from which the graph should be created!" << endl;
+    //cin >> file_name;
+    //cout << file_name << endl;
+    ifstream word_file(file_name);
+    Graph test_graph(word_file, 1);
+    test_graph.print();
+
+    float dist_min_spanning_tree = test_graph.minimum_spanning_tree();
+    cout << "minimal spanning tree has length dist: " << dist_min_spanning_tree << endl;
 }
